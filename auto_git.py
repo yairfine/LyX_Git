c39018255af7edb958ee685df25c00ddbb3d13ef
yair Fine <yair.yfs@gmail.com>
2020-09-25 15:39:07 +0300

    updates

--- auto_git.py
+++ auto_git.py
@@ -1,8 +1,15 @@
 import os
 import pathlib
 import time
-import argparse
+from argparse import ArgumentParser
+
+FILE_PATH = "The path to the file you want to git."
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("file_path", help=FILE_PATH)
 
 if __name__ == "__main__":
     print("hello man!")
+    main()
 
