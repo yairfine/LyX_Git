a1aee97887ed376f7ccd19e1497a229fc77c321e
yair Fine <yair.yfs@gmail.com>
2020-09-25 16:40:31 +0300

    after ssh

--- auto_git.py
+++ auto_git.py
@@ -5,11 +5,12 @@ from argparse import ArgumentParser
 
 FILE_PATH = "The path to the file you want to git."
 
+
 def main():
     parser = ArgumentParser()
     parser.add_argument("file_path", help=FILE_PATH)
 
+
 if __name__ == "__main__":
     print("hello man!")
     main()
-
