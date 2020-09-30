7072ac375b6d66cec73bf97dc46ec4dd018a9a07
yair Fine <yair.yfs@gmail.com>
2020-10-01 00:20:32 +0300

    first config works

--- auto_git.py
+++ auto_git.py
@@ -8,9 +8,11 @@ from argparse import ArgumentParser
 from pathlib import Path
 from git import Repo # ? https://www.devdungeon.com/content/working-git-repositories-python
 import git
+from pprint import pprint
+import subprocess
 
 DESCRIPTION = "Track file and automatic push to remote github repository."
-EPILOG = "For more information, visit the project page on: github.com/yairfine/auto-git"
+EPILOG = "For more information, visit the project page on: https://github.com/yairfine/auto-git"
 HELP_FILE_PATH = "The path to the file you want to git."
 HELP_START_TRACK = "Sets the program to track an existing repo and file"
 HELP_NEW_TRACK = "Sets the program create a new track for a given file"
@@ -20,29 +22,105 @@ METAVAR_FILE_PATH = "<file_path>"
 ERR_PAT_EXISTS = "It's seems like you already configured this system, try to run again with -n/-s flag"
 
 MSG_EXIT = "To stop: press Ctrl+C and wait a few seconds"
+MSG_SSH_PAT_CONFIG = """
+Make sure you have Git installed (version > 1.5)
+2.  Open: Git-Bash
+3.  Run: ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
+4.  Press enter: Enter a file in which to save the key (/c/Users/you/.ssh/id_rsa):[Press enter]
+5.  Press enter: Enter passphrase (empty for no passphrase): [Press enter] (no password needed)
+6.  Press enter: [Press enter again] (no password needed)
+7.  Run: eval $(ssh-agent -s)
+8.  Run: ssh-add ~/.ssh/id_rsa
+9.  Run: clip < ~/.ssh/id_rsa.pub   #Copies the contents of the id_rsa.pub file to your clipboard
+10. Open: github.com
+11. Click: In upper-right corner click profile photo -> settings
+12. Click: SSH and GPG keys (side menu)
+13. Click: New SSH key or Add SSH key.
+14. Title field: write 'auto-git-ssh'
+15. Key field: paste your key
+16. Click: Add SSH key
+17. Click: Developer settings (side menu)
+18. Click: Personal access tokens
+19. Click: Generate new token
+20. Note field: write 'auto-git-pat'
+21. Select scopes: 'repo', 'read:user', 'user:email'
+22. Click: Generate token
+23. COPY!!!: copy the new token (with green V sign aside)
+24. Paste: paste the token here and press enter: 
+"""
 
 SETTINGS_DIR = Path.home() / 'auto-git-settings'
-SETTINGS_SSH = SETTINGS_DIR / 'ssh.txt'
-SETTINGS_PAT = SETTINGS_DIR / 'pat.txt'
-SETTINGS_USER = SETTINGS_DIR / 'user.txt'
+SETTINGS_FILE_GLOBAL = SETTINGS_DIR / 'auto_git_settings_global.txt'
+# SETTINGS_PAT = SETTINGS_DIR / 'pat.txt'
+# SETTINGS_USER = SETTINGS_DIR / 'user.txt'
+
+BASE_URL = 'https://api.github.com'
+
 
 def initiate_settings_dir(settings_dir_path):
-    
+
     try:
         settings_dir_path.mkdir()
 
-        SETTINGS_SSH.touch(exist_ok=False)
-        SETTINGS_PAT.touch(exist_ok=False)
+        SETTINGS_FILE_GLOBAL.touch(exist_ok=False)
 
     except FileExistsError:
         print(ERR_PAT_EXISTS)
         sys.exit()
 
-    # todo pat = input("Please enter you Private Token Authenticator: ")
-    # SETTINGS_PAT.write_text(pat ,encoding=UTF-8)
+    pat = input(MSG_SSH_PAT_CONFIG)
+
+    headers = {
+        "Authorization" : f"token {pat}"
+    }
     
-    # todo create a username and email file!
-    # todo config them --global !!!
+
+    end_point = "/user"
+    url = f"{BASE_URL}{end_point}"
+
+    r = requests.get(url, headers=headers)
+    
+    if not r.ok:
+        print("r is not ok - ", r.status_code)
+        sys.exit()
+    try:
+        response_data = json.loads(r.text)
+    except:
+        print("cannot parse json")
+        sys.exit()
+
+    user_name = response_data['login']
+
+    end_point = "/user/emails"
+    url = f"{BASE_URL}{end_point}"
+
+    r = requests.get(url, headers=headers)
+    
+    if not r.ok:
+        print("r is not ok - ", r.status_code)
+        sys.exit()
+    try:
+        response_data = json.loads(r.text)
+    except:
+        print("cannot parse json")
+        sys.exit()
+        
+    user_email = response_data[0]['email']
+
+    settings_dict_global = {
+        "PAT" : f"{pat}",
+        "user_name" : f"{user_name}",
+        "user_email" : f"{user_email}"
+    }
+    
+    settings_json = json.dumps(settings_dict_global)
+    SETTINGS_FILE_GLOBAL.write_text(settings_json)
+
+    ret = subprocess.run(f"git config --global user.name {user_name}")
+    ret = subprocess.run(f"git config --global user.email {user_email}")
+
+    # * todo create a username and email file!
+    # * todo config them --global !!!
     # todo add github to the list of known-hosts. handle it before pushes!
 
 async def push_changes(file_to_track):
@@ -113,16 +191,17 @@ def new_track(raw_file_path):
     "private" : "true"
     }
     
-    pat = SETTINGS_PAT.read_text()
+    settings_json_global = SETTINGS_FILE_GLOBAL.read_text()
+    settings_dict_global = json.loads(settings_json_global.read_text())
+
 
     headers = {
-        "Authorization" : f"token {pat}"
+        "Authorization" : f"token {settings_dict_global['PAT']}"
     }
 
-    base_url = 'https://api.github.com'
     end_point = "/user/repos"
 
-    url = f"{base_url}{end_point}"
+    url = f"{BASE_URL}{end_point}"
 
     r = requests.post(url, headers=headers, json=json_data)
     
@@ -131,7 +210,7 @@ def new_track(raw_file_path):
         sys.exit()
 
     try:
-        response_data = r.json()
+        response_data = json.loads(r.text)
     except:
         print("an error accord while trying create the repo on github")
         sys.exit()
@@ -196,9 +275,9 @@ def main():
     parser = ArgumentParser(description=DESCRIPTION, epilog=EPILOG)
     
     group = parser.add_mutually_exclusive_group(required = True)
-    group.add_argument('-s', '--start-track', action="store", type=str, help=HELP_START_TRACK)
-    group.add_argument('-n', '--new-track', action="store", type=str, help=HELP_NEW_TRACK)
-    group.add_argument('-c', '--first-config', action="store_true", help=HELP_FIRST_CONFIG)
+    group.add_argument('-f', '--first-config', action="store_true", help=HELP_FIRST_CONFIG)
+    group.add_argument('-n', '--new-track', action="store", type=str, help=HELP_NEW_TRACK, metavar=METAVAR_FILE_PATH)
+    group.add_argument('-s', '--start-track', action="store", type=str, help=HELP_START_TRACK, metavar=METAVAR_FILE_PATH)
 
     # parser.add_argument("file_path", help=HELP_FILE_PATH, metavar=METAVAR_FILE_PATH)
 
@@ -210,7 +289,6 @@ def main():
     elif args.new_track is not None:
         new_track(args.new_track)
     
-    #todo first config no need for path
     elif args.first_config is not None:
         first_config()
 
