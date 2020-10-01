633aad0dfd4851dfe1048a6562e4e5ee93d8f9b0
yair Fine <yair.yfs@gmail.com>
2020-10-01 14:46:23 +0300

    cosmetic changes

--- auto_git.py
+++ auto_git.py
@@ -12,6 +12,8 @@ import git
 from pprint import pprint
 import subprocess
 
+INTERVAL_SECONDS = 7  # 60 in deployment version
+
 DESCRIPTION = "Track file and automatic push to remote github repository."
 EPILOG = "For more information, visit the project page on: https://github.com/yairfine/auto-git"
 HELP_FILE_PATH = "The path to the file you want to git."
@@ -21,9 +23,19 @@ HELP_FIRST_CONFIG = "Sets the program to first-config mode"
 METAVAR_FILE_PATH = "<file_path>"
 
 ERR_PAT_EXISTS = "It's seems like you already configured this system, try to run again with -n/-s flag"
+ERR_CREATE_REMOTE = ' ~~ Error creating remote repo ~~ '
+ERR_SETTINGS_LOCAL_EXISTS = "It's seems like you already initiated this directory, try to run again with -s flag"
+ERR_PARSE_JSON = " ~~ Error parsing json ~~ "
+ERR_STATUS_CODE = "Respone code is not ok - {}"
 
-MSG_EXIT = "To stop: press Ctrl+C and wait a few seconds"
-MSG_SSH_PAT_CONFIG = """
+MSG_END_NEW_TRACK = "Done preparing for a new track"
+MSG_START_TRACKING = "Started tracking changes on file '{}'"
+MSG_EXIT_TRACKING = "To stop: press Ctrl+C and wait a few seconds"
+MSG_END_TRACKING = "Tracking session has ended"
+MSG_CHANGE_RECORDED = "A change was recorded in {}"
+MSG_COMMIT = "commit no.{} - {}"
+
+PROMPT_SSH_PAT_CONFIG = """
 Make sure you have Git installed (version > 1.5)
 2.  Open: Git-Bash
 3.  Run: ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
@@ -47,15 +59,17 @@ Make sure you have Git installed (version > 1.5)
 21. Select scopes: 'repo', 'read:user', 'user:email'
 22. Click: Generate token
 23. COPY!!!: copy the new token (with green V sign aside)
-24. Paste: paste the token here and press enter: 
+24. Paste: paste the token here and press enter:
 """
+PROMPT_PAT = "Please enter your Private Accesses Token: "
+PROMPT_REPO_NAME = "Please enter your new repository name: "
 
 SETTINGS_DIR = Path.home() / 'auto-git-settings'
 SETTINGS_FILE_GLOBAL = SETTINGS_DIR / 'auto_git_settings_global.txt'
 # SETTINGS_PAT = SETTINGS_DIR / 'pat.txt'
 # SETTINGS_USER = SETTINGS_DIR / 'user.txt'
 
-BASE_URL = 'https://api.github.com'
+API_BASE_URL = 'https://api.github.com'
 
 
 def initiate_settings_global_dir(settings_dir_path):
@@ -70,12 +84,12 @@ def initiate_settings_global_dir(settings_dir_path):
 
 
 def retrieve_pat():
-    pat = input(MSG_SSH_PAT_CONFIG)
+    pat = input(PROMPT_PAT)
     return pat
 
 
 def get_endpoint(end_point, pat):
-    url = f"{BASE_URL}{end_point}"
+    url = f"{API_BASE_URL}{end_point}"
 
     headers = {
         "Authorization": f"token {pat}"
@@ -84,13 +98,36 @@ def get_endpoint(end_point, pat):
     r = requests.get(url, headers=headers)
 
     if not r.ok:
-        print("r is not ok - ", r.status_code)
+        print(ERR_STATUS_CODE.format(r.status_code))
         sys.exit()
 
     try:
         response_dict = json.loads(r.text)
     except:
-        print("cannot parse json")
+        print(ERR_PARSE_JSON)
+        sys.exit()
+
+    return response_dict
+
+
+def post_endpoint(end_point, pat, json_data):
+    url = f"{API_BASE_URL}{end_point}"
+
+    headers = {
+        "Authorization": f"token {pat}"
+    }
+
+    r = requests.post(url, headers=headers, json=json_data)
+
+    if r.status_code != 201:
+        print(ERR_CREATE_REMOTE)
+        print(ERR_STATUS_CODE.format(r.status_code))
+        sys.exit()
+
+    try:
+        response_dict = json.loads(r.text)
+    except:
+        print(ERR_CREATE_REMOTE)
         sys.exit()
 
     return response_dict
@@ -101,25 +138,25 @@ async def push_changes(file_to_track):
     settings_file = dir_path / 'auto_git_settings.txt'
     settings_dict = json.loads(settings_file.read_text())
 
-    print(f"Started tracking changes on file '{settings_dict['file_name']}'")
-    print(MSG_EXIT)
+    print(MSG_START_TRACKING.format(settings_dict['file_name']))
+    print(MSG_EXIT_TRACKING)
 
     repo = Repo(dir_path)
 
     while True:
-        await asyncio.sleep(7)
+        await asyncio.sleep(INTERVAL_SECONDS)
 
         if repo.is_dirty(untracked_files=True):
 
             repo.git.add('.')
-            repo.index.commit(
-                f"commit no.{settings_dict['count_commits']} - {time.asctime(time.localtime())}")
+            repo.index.commit(MSG_COMMIT.format(settings_dict['count_commits'],
+                                                time.asctime(time.localtime())))
             repo.remotes.origin.push()
 
             settings_dict['count_commits'] += 1
             settings_json = json.dumps(settings_dict)
             settings_file.write_text(settings_json)
-            print("A change was recorded")
+            print(MSG_CHANGE_RECORDED.format(time.asctime(time.localtime())))
 
 
 def start_track(raw_file_path):
@@ -132,7 +169,7 @@ def start_track(raw_file_path):
     except KeyboardInterrupt:
         pass
     finally:
-        print("Tracking session has ended")
+        print(MSG_END_TRACKING)
         # maybe update some settings here?
         loop.close()
 
@@ -142,70 +179,26 @@ def start_track(raw_file_path):
     # todo add in json all the files we track
 
 
-def new_track(raw_file_path):
-    file_to_track = Path(raw_file_path)
-    dir_path = file_to_track.parent
-
+def initiate_settings_local_dir(settings_file, readme_file, gitignore_file):
     try:
-        settings_file = dir_path / 'auto_git_settings.txt'
-        readme_file = dir_path / 'README.md'
-        gitignore_file = dir_path / '.gitignore'
         settings_file.touch(exist_ok=False)
         readme_file.touch(exist_ok=False)
         gitignore_file.touch(exist_ok=False)
 
     except FileExistsError:
-        print("It's seems like you already intiated this directory, try to run again with -s flag")
+        print(ERR_SETTINGS_LOCAL_EXISTS)
         sys.exit()
 
-    repo_name = input("Please enter your new repository name: ")
 
-    json_data = {
-        "name": f"{repo_name}",
-        "private": "true"
-    }
-
-    settings_json_global = SETTINGS_FILE_GLOBAL.read_text()
-    settings_dict_global = json.loads(settings_json_global.read_text())
-
-    headers = {
-        "Authorization": f"token {settings_dict_global['PAT']}"
-    }
-
-    end_point = "/user/repos"
-
-    url = f"{BASE_URL}{end_point}"
-
-    r = requests.post(url, headers=headers, json=json_data)
-
-    if r.status_code != 201:
-        print("an error accord while trying create the repo on github")
-        sys.exit()
-
-    try:
-        response_data = json.loads(r.text)
-    except:
-        print("an error accord while trying create the repo on github")
-        sys.exit()
-
-    ssh_url = response_data['ssh_url']
-    https_url = response_data['clone_url']
-
-    settings_dict = {
-        "file_name": f"{file_to_track.name}",
-        "repo_name": f"{repo_name}",
-        "ssh_url": f"{ssh_url}",
-        "https_url": f"{https_url}",
-        "count_commits": 1
-    }
-    settings_json = json.dumps(settings_dict)
+def write_settings_local(settings_file, settings_json, readme_file, readme,
+                         gitignore_file, ignores):
     settings_file.write_text(settings_json)
+    readme_file.write_text(readme)
+    gitignore_file.write_text(ignores)
 
-    gitignore_file.write_text("auto_git_settings.txt")
-    readme_file.write_text(f"# {repo_name}")
 
+def first_init_add_commit_push(dir_path, settings_dict_local):
     new_repo = Repo.init(path=dir_path, mkdir=False)
-    # new_repo = Repo(path=dir_path)
 
     new_repo.git.add('.')
     new_repo.index.commit('Initial commit.')
@@ -213,13 +206,53 @@ def new_track(raw_file_path):
     new_repo.create_head('master').checkout()
 
     try:
-        origin = new_repo.create_remote('origin', url=settings_dict['ssh_url'])
-
+        origin = new_repo.create_remote(
+            'origin', url=settings_dict_local['ssh_url'])
     except:
-        print(' ~~~ Error creating remote ~~~ ')
+        print(ERR_CREATE_REMOTE)
         sys.exit()
 
     new_repo.git.push("--set-upstream", origin, new_repo.head.ref)
+
+
+def new_track(raw_file_path):
+    file_to_track = Path(raw_file_path)
+    dir_path = file_to_track.parent
+    settings_file = dir_path / 'auto_git_settings.txt'
+    readme_file = dir_path / 'README.md'
+    gitignore_file = dir_path / '.gitignore'
+
+    initiate_settings_local_dir(settings_file, readme_file, gitignore_file)
+
+    repo_name = input(PROMPT_REPO_NAME)
+
+    json_data = {
+        "name": f"{repo_name}",
+        "private": "true"
+    }
+
+    settings_dict_global = json.loads(SETTINGS_FILE_GLOBAL.read_text())
+
+    response_dict = post_endpoint(
+        "/user/repos", settings_dict_global['PAT'], json_data)
+
+    settings_dict_local = {
+        "file_name": f"{file_to_track.name}",
+        "repo_name": f"{repo_name}",
+        "ssh_url": f"{response_dict['ssh_url']}",
+        "https_url": f"{response_dict['clone_url']}",
+        "count_commits": 1
+    }
+
+    write_settings_local(settings_file, json.dumps(settings_dict_local),
+                         gitignore_file, "auto_git_settings.txt",
+                         readme_file, f"# {repo_name}")
+
+    first_init_add_commit_push(dir_path, settings_dict_local)
+
+    print(MSG_END_NEW_TRACK)
+
+    start_track(raw_file_path)
 
     # * create a dict to json and retrieve https://stackoverflow.com/questions/26745519/converting-dictionary-to-json
     # * create settings file with the repo name and repo ssh https URI
@@ -227,6 +260,7 @@ def new_track(raw_file_path):
     # * todo touch .gitignore file
     # * todo git init add commit
     # * todo create new ssh remote tracking branch
+    # todo make sure that github is in known hosts
     # todo and than start_track()
 
 
@@ -254,8 +288,8 @@ def first_config():
 
     ret = subprocess.run(f"git config --global user.name {user_name}")
     ret = subprocess.run(f"git config --global user.email {user_email}")
+    #todo - check the ret
 
-    
     # todo add github to the list of known-hosts. handle it before pushes!
 
     # todo generate ssh key, store it on place, and copy the pass to ssh.txt
@@ -275,8 +309,6 @@ def main():
                        type=str, help=HELP_NEW_TRACK, metavar=METAVAR_FILE_PATH)
     group.add_argument('-s', '--start-track', action="store",
                        type=str, help=HELP_START_TRACK, metavar=METAVAR_FILE_PATH)
-
-    # parser.add_argument("file_path", help=HELP_FILE_PATH, metavar=METAVAR_FILE_PATH)
 
     args = parser.parse_args()
 
